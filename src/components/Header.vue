c7fd880cc969412c9ec504812a0f745b476f1656
Flasky06 <bonnienjuguna106@gmail.com>
2023-09-14 09:43:38 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+	<header>
+		<h1>{{ "Task tracker " }}</h1>
+		<AddTaskButton text="Add Task" color="green" />
+	</header>
+</template>
+
+<script>
+import AddTaskButton from "./Button.vue";
+
+export default {
+	name: "AppHeader",
+	props: { title: { type: String, default: "Hello World" } },
+	components: { AddTaskButton },
+};
+</script>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+</style>
---
+++ src/components/TasksComponent.vue
@@ -0,0 +1,14 @@
+<template>
+	<div :key="task.id" v-for="task in tasks">
+		<TaskComponent :task="task" />
+	</div>
+</template>
+
+<script>
+import TaskComponent from "./TaskComponent.vue";
+export default {
+	name: "TasksComponent",
+	props: { tasks: Array },
+	components: { TaskComponent },
+};
+</script>
